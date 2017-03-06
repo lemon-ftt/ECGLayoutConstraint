1912632ffc4e39e1a992d7b482d1c2fd8a063425
Fighter-FT <tan@yanyandeMac-mini.local>
2017-03-06 15:13:52 +0800

    ECGLayoutConstraintDemo
    根据比例设置约束

---
+++ ECGLayoutConstraintDemo/ECGLayoutConstraintDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ECGLayoutConstraintDemo
+//
+//  Created by tan on 2017/3/6.
+//  Copyright © 2017年 tantan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
